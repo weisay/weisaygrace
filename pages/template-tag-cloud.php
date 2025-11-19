13afcf59d3a8c07a7a2a4503985daf6501936ab8
weisay <wxwvv@163.com>
2025-11-19 16:50:10 +0800

    v2.2.0

--- pages/template-tag-cloud.php
+++ pages/template-tag-cloud.php
@@ -7,7 +7,7 @@ Template Name: 标签云集
 <div class="container">
 <div class="main main-all">
 <div class="crumb">当前位置： <a title="返回首页" href="<?php bloginfo('url'); ?>/">首页</a> &gt; <h1><?php the_title(); ?></h1></div>
-<div class="article article-tag-cloud">
+<div class="article page-tag">
 <?php echo tag_cloud_list(); ?>
 </div>
 </div>
